5fb601212d54edc7f1f3d9229c4683eaf861c5f2
rath <rath@xrath.com>
2024-12-28 21:52:45 +0100

    feat: Add admin configuration for YouTubeVideo and Settings models

--- api/apps/admin.py
+++ api/apps/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import YouTubeVideo, Settings
 
-# Register your models here.
+
+@admin.register(YouTubeVideo)
+class YouTubeVideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'video_id', 'duration', 'width', 'height')
+    search_fields = ('title', 'video_id')
+    readonly_fields = ('video_id', 'width', 'height', 'duration')
+    list_filter = ('height', 'width')
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ('max_video_height', 'use_he_aac_v2')
+    
+    def has_add_permission(self, request):
+        # Prevent creating multiple Settings instances
+        return not Settings.objects.exists()
+
+    def has_delete_permission(self, request, obj=None):
+        # Prevent deleting the Settings instance
+        return False
