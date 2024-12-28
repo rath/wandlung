430b6a999873e854387748670830c96d30a8d9b9
rath <rath@xrath.com>
2024-12-28 18:20:49 +0100

    Add video download endpoint and yt-dlp dependency
    - Introduce /videos/download endpoint using yt-dlp
    - Save downloaded video and thumbnail in the database
    - Clean up thumbnail file after saving
    - Add yt-dlp to pyproject.toml dependencies

--- api/apps/api.py
+++ api/apps/api.py
@@ -1,10 +1,63 @@
-from ninja import NinjaAPI
+import datetime
+import os
+import urllib.request
+from tempfile import NamedTemporaryFile
 
+import yt_dlp
+from PIL import Image
+from django.core.files import File
+from ninja import NinjaAPI, Schema
+
+from apps.models import YouTubeVideo
 
 api = NinjaAPI()
 
 
-@api.get('/ping')
+class VideoDownloadRequest(Schema):
+    url: str
+
+
+@api.post('/videos/download')
+def download_video(request, payload: VideoDownloadRequest):
+    url = payload.url
+    ydl_opts = {
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
+        'merge_output_format': 'mp4',
+        'outtmpl': 'videos/%(id)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        video_id = info.get("id", None)
+        seconds = info.get('duration', None)
+        thumbnail_url = info.get('thumbnail', None)
+        video_path = ydl.prepare_filename(info)
+
+    # Download thumbnail
+    thumbnail_path = f'videos/{video_id}.jpg'
+    with NamedTemporaryFile(delete=False) as temp_file:
+        with urllib.request.urlopen(thumbnail_url) as response:
+            temp_file.write(response.read())
+        temp_file.flush()
+        with Image.open(temp_file.name) as img:
+            img.save(thumbnail_path)
+
+    # Save to database
+    with open(video_path, 'rb') as video_file, open(thumbnail_path, 'rb') as thumb_file:
+        video = YouTubeVideo.objects.create(
+            video_id=video_id,
+            thumbnail=File(thumb_file),
+            duration=datetime.timedelta(seconds=seconds),
+            width=info.get('width', None),
+            height=info.get('height', None),
+            title=info.get('title', None),
+            original_video=File(video_file)
+        )
+
+    # Clean up
+    os.remove(thumbnail_path)
+    return {'video_id': video.video_id}
+
+
 def ping(request):
     return {'message': 'pong'}
 
